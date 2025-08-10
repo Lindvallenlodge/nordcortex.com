f347d3e401fa65b722791f98a4ea1e545cbe5a42
Lindvallenlodge <audas.j@gmail.com>
2025-08-11 01:33:50 +0200

    Update order.js

--- assets/js/order.js
+++ assets/js/order.js
@@ -4,26 +4,4 @@
     margin-top: 10px;
     margin-bottom: 10px;
   }
-  .date-time-box {
-    display: inline-block;
-    width: 100%;
-    padding: 8px 12px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    background-color: #fff;
-    box-sizing: border-box;
-  }
-}
-
-/* Desktop styles */
-@media (min-width: 769px) {
-  .date-time-box {
-    display: inline-block;
-    width: auto;
-    padding: 8px 12px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    background-color: #fff;
-    box-sizing: border-box;
-  }
 }
